ff1217970789e1a25434373f9d55ae1022f08818
som-m <chanapa.pntt@hotmail.com>
2021-06-06 11:58:35 +0700

    use random order id instead

--- checkout.php
+++ checkout.php
@@ -1,39 +1,17 @@
 <?php
 require_once 'config.php';
 
-$file = "orderid.txt";
-$linecount = 0;
-$handle = fopen($file, "r");
-while (!feof($handle)) {
-	$line = fgets($handle);
-	$linecount++;
-}
-fclose($handle);
-$order_id = $linecount;
+$order_id = date('YmdHi') . rand(1000, 9999);
 
-$metadata = array(
-	'order_id'	=> $order_id,
-	'OmisePHP'	=> '2.13.0'
-);
-
-$attrs = array(
+$charge = OmiseCharge::create(array(
 	'amount'		=> 10000,
 	'currency'		=> 'thb',
 	'return_uri'	=> 'https://merchant-site.com/complete.php/orderid=' . $order_id,
 	'description'	=> 'Test payment. Order ID: ' . $order_id,
-	'metadata'		=> $metadata
-);
-
-if ($_POST['omiseSource']) {
-	$attrs['source'] = $_POST['omiseSource'];
-}
-
-if ($_POST['omiseToken']) {
-	$attrs['card'] = $_POST['omiseToken'];
-}
-
-$charge = OmiseCharge::create($attrs);
+	'metadata'		=> array(
+		'order_id'	=> $order_id
+	)
+	'card'			=> $_POST['omiseToken'];
+));
 
 header('Location: ' . $charge['authorize_uri']);
-
-file_put_contents("orderid.txt", "\n" . $charge['id'] . "=" . $order_id, FILE_APPEND);
--- complete.php
+++ complete.php
@@ -6,7 +6,7 @@ $path = $_SERVER['PATH_INFO'];
 $order_id = filter_var($path, FILTER_SANITIZE_NUMBER_INT);
 
 $search = OmiseCharge::search($order_id)->filter(array(
-	'created' => "today",
+	'created' => 'today',
 ));
 
 $charge_id = $search['data'][0]['id'];
@@ -14,7 +14,7 @@ $charge_id = $search['data'][0]['id'];
 $charge = OmiseCharge::retrieve($charge_id);
 
 if ( $charge['authorized'] == false || $charge['paid'] == false) {
-	echo $charge['id'] . ": failed";
+	echo $charge['id'] . ': ' . $charge['failure_message'];
 } else {
-	echo $charge['id'] . ": successful";
+	echo $charge['id'] . ': ' . $charge['status'];
 }
