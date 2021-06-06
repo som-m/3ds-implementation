5eadf73eed6915b058e9e271e47c8c578545c1fe
som-m <chanapa.pntt@hotmail.com>
2021-06-06 18:06:25 +0700

    format code

--- checkout.php
+++ checkout.php
@@ -5,14 +5,14 @@ require_once 'config.php';
 $order_id = date('YmdHis') . rand(100, 999);
 
 $charge = OmiseCharge::create(array(
-	'amount'			=> 10000,
-	'currency'		=> 'thb',
-	'return_uri'	=> 'http://localhost:8080/complete.php/orderid=' . $order_id,
-	'description'	=> 'Test payment from som-m/3ds-implementation',
-	'metadata'		=> array(
-		'order_id'	=> $order_id
-	),
-	'card'				=> $_POST['omiseToken']
+  'amount'      => 10000,
+  'currency'    => 'thb',
+  'return_uri'  => 'http://localhost:8080/complete.php/orderid=' . $order_id,
+  'description' => 'Test payment from som-m/3ds-implementation',
+  'metadata'    => array(
+    'order_id'  => $order_id
+  ),
+  'card'        => $_POST['omiseToken']
 ));
 
 header('Location: ' . $charge['authorize_uri']);
--- complete.php
+++ complete.php
@@ -13,7 +13,7 @@ $charge_id = $search['data'][0]['id'];
 $charge = OmiseCharge::retrieve($charge_id);
 
 if ( $charge['authorized'] == false || $charge['paid'] == false) {
-	echo $charge['id'] . ': ' . $charge['failure_message'];
+  echo $charge['id'] . ': ' . $charge['failure_message'];
 } else {
-	echo $charge['id'] . ': ' . $charge['status'];
+  echo $charge['id'] . ': ' . $charge['status'];
 }
