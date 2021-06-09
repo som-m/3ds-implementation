5e5a322bb28d01af52fb6e7efdb9a7f0ab4fb3c9
som-m <chanapa.pntt@hotmail.com>
2021-06-09 20:08:45 +0700

    Add installment

--- checkout.php
+++ checkout.php
@@ -5,7 +5,7 @@ require_once 'config.php';
 $order_id = date('YmdHis') . rand(100, 999);
 
 $charge = OmiseCharge::create(array(
-  'amount'      => 10000,
+  'amount'      => 300000,
   'currency'    => 'thb',
   'return_uri'  => 'http://localhost:8080/complete.php/orderid=' . $order_id,
   'description' => 'Test payment from som-m/omise-php-example',
