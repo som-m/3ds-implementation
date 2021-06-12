2e63184d813f0c35e057f5184d7713bf57eb315d
som-m <chanapa.pntt@hotmail.com>
2021-06-12 15:09:11 +0700

    Use single quotes everywhere

--- app/webhook.php
+++ app/webhook.php
@@ -4,8 +4,8 @@ $payload = file_get_contents('php://input');
 
 $event = json_decode($payload);
 
-if ($event->key == "charge.complete") {
+if ($event->key == 'charge.complete') {
   $charge_status = $event->data->id . '=' . $event->data->status;
 
-  file_put_contents("status.txt", PHP_EOL . $charge_status, FILE_APPEND);
+  file_put_contents('status.txt', PHP_EOL . $charge_status, FILE_APPEND);
 }
