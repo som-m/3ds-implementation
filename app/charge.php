4b6dac77535ce48c87f0161fa5303c202bdb06fa
som-m <chanapa.pntt@hotmail.com>
2021-06-13 21:23:21 +0700

    Add update_status method

--- app/charge.php
+++ app/charge.php
@@ -9,11 +9,35 @@ class Charge {
     while (($row = fgetcsv($file)) !== FALSE) {
       if ($row[0] == $id || $row[1] == $id) {
         $this->charge = array($row[0], $row[1], $row[2], $row[3], $row[4]);
+
         break;
       }
     }
 
     fclose($file);
+  }
+
+  function update_status($id, $status) {
+    $file = fopen('charge.csv', 'r');
+    $file_temp = fopen('charge-temp.csv', 'w');
+
+    while (($row = fgetcsv($file)) !== FALSE) {
+      if ($row[1] == $id) {
+        $row[4] = $status;
+      }
+      fputcsv($file_temp, $row);
+    }
+
+    $stat = fstat($file_temp);
+
+    ftruncate($file_temp, $stat['size']-1);
+
+    fclose($file);
+    fclose($file_temp);
+
+    unlink('charge.csv');
+
+    rename('charge-temp.csv', 'charge.csv');
   }
 
   function order_id() {
--- app/complete.php
+++ app/complete.php
@@ -2,30 +2,13 @@
 
 include_once __dir__ . '/../app/charge.php';
 
-$charge = new Charge();
-$charge->get($order_id);
-$charge_id = $charge->charge_id();
+$order = new Charge();
+$order->get($order_id);
+
+$charge_id = $order->charge_id();
 
 $charge = OmiseCharge::retrieve($charge_id);
 
+$order->update_status($charge['id'], $charge['status']);
+
 header('Location: /../' . $charge['id'] . '/status');
-
-$input = fopen('charge.csv', 'r');
-$output = fopen('status-temp.csv', 'w');
-
-while (($row = fgetcsv($input)) !== FALSE) {
-  if ($row[1] == $charge['id']) {
-    $row[4] = $charge['status'];
-  }
-  fputcsv($output, $row);
-}
-
-$stat = fstat($output);
-ftruncate($output, $stat['size']-1);
-
-fclose($input);
-fclose($output);
-
-unlink('charge.csv');
-
-rename('status-temp.csv', 'charge.csv');
