b1fdec52f85f6e85531da5ea1b2444eab70632e9
Chanapa Panithantham <som@Namsoms-Mac.local>
2022-04-01 17:11:36 +0700

    Add payment_type in metadata

--- app/checkout.php
+++ app/checkout.php
@@ -14,21 +14,21 @@ $attrs = array(
 );
 
 if ($_POST['omiseToken']) {
-  $attrs['description'] = 'card';
+  $attrs['metadata']['payment_type'] = 'card';
   $attrs['card'] = $_POST['omiseToken'];
 }
 
 if ($_POST['omiseSource']) {
   $source = OmiseSource::retrieve($_POST['omiseSource']);
 
-  $attrs['description'] = $source['type'];
+  $attrs['metadata']['payment_type'] = $source['type'];
   $attrs['source'] = $_POST['omiseSource'];
 }
 
 $charge = OmiseCharge::create($attrs);
 
 $charge_id = $charge['id'];
-$paymemt_type = $charge['description'];
+$paymemt_type = $charge['metadata']['payment_type'];
 $barcode = $charge['source']['references']['barcode'];
 $status = $charge['status'];
 
